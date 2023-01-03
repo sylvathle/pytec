f2f678ea3d353ee4728f700646ed36caca268e46
Sylvain Blunier <sylvain.blunier@gmail.com>
2023-01-03 19:06:49 -0300

    files removed from git repository

--- test-consume-package.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-# https://packaging.python.org/guides/distributing-packages-using-setuptools/#working-in-development-mode
-
-python3 ./package-consumer-project/consumepackage.py
-source ~/.bashrc
